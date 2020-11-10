3a90a14b9d1807f0f8f5facb1571c7abb99b8e88
Serhii <sergs123456789@gmail.com>
2020-11-10 16:09:19 +0200

    Added life cycle methods componentDidMount and componentWillUnmount

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
 import AddPanel from './components/AddPanel/AddPanel';
 import { Header } from './components/Header/Header';
 import { List } from './components/List/List';
 import SearchPanel from './components/SearchPanel/SearchPanel';
+import { Clock } from './components/Clock/Clock';
 
-export const App = () => {
-  return (
-    <div className='App'>
-      <Header/>
-      <div className='panels'>
-        <AddPanel/>
-        <SearchPanel/>
-      </div>
-      <List/>
-  </div>
-  );
+function getFormattedTime() {
+  let time = new Date().toLocaleTimeString();
+  return time;
 }
+
+export class App extends Component {
+  state = {
+    time: null
+  }
+  clock = () => {
+    this.setState({
+      time: getFormattedTime()
+    })
+  };
+  componentDidMount(){
+    console.log('---> componentDidMount()')
+    this.clock()
+    // this.interval = setInterval(this.clock, 1000);
+  }
+  componentWillUnmount(){
+    console.log('---> componentWillUnmount()')
+    clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <div className='App'>
+        {console.log('---> render()')}
+        <Header/>
+        {/* <Clock time={this.state.time}/> */}
+        <div className='panels'>
+          <AddPanel/>
+          <SearchPanel/>
+        </div>
+        <List/>
+    </div>
+    );
+  }
+}
+
 
 
