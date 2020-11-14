9ca32169786f64388c70fc38b06c86ceefd8f509
Serhii <sergs123456789@gmail.com>
2020-11-14 17:50:19 +0200

    added component warning

--- todolist/src/components/Warning/Warning.js
+++ todolist/src/components/Warning/Warning.js
@@ -2,7 +2,7 @@ import React from 'react'
 import  './Warning.css';
 export const Warning = ({children}) => (
         <p className='warning'>
-            <span class="material-icons" style={{color: 'inherit', marginRight: '5px'}}>
+            <span className="material-icons" style={{color: 'inherit', marginRight: '5px'}}>
                 report_problem
             </span>
             {children}
