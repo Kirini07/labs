137ce1f638c48cb70555e8b22f10f5fc5477c9fa
Serhii <sergs123456789@gmail.com>
2020-11-17 13:33:24 +0200

    Added methods for deleting adding changing priorities changing task status and did filtering and searching

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,17 +4,51 @@ import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 import { Header } from './components/Header/Header';
 import { List } from './containers/List/List';
-import { Clock } from './components/Clock/Clock';
+// import { Clock } from './components/Clock/Clock';
 import { Form } from './components/Form/Form';
 import { SketchPicker } from 'react-color';
 
 
 export class App extends Component {
   state = {
+    search: '',
+    filter: 'all',
     tasks: []
   }
-  onSearch(obj){
+  filter(arr, filter){
+    switch(filter) {
+      case 'all'
+        : return arr; 
 
+      case 'active'
+        : return arr.filter(e => !e.done);
+      
+      case 'done'
+        : return arr.filter(e => e.done);
+
+      case 'important'
+        : return arr.filter(e => e.important);
+      
+      default
+        : return arr;
+    }
+  }
+  search(arr, search = ''){
+    if (search.trim().length === 0) {
+      return arr;
+    }
+    return arr.filter(e => {
+      return e.title.toLowerCase().indexOf(search.toLowerCase()) > -1;
+    });
+  }
+  onFliterChange = filter => {
+    this.setState({
+      filter
+    })
+  }
+  onSearch = ({search}) =>{
+    this.setState({ search });
+    console.log(search)
   }
   onChangeAtribute = (attr, id) => {
     this.setState((state) =>{
@@ -68,23 +102,25 @@ export class App extends Component {
     })
   }
   render() {
+    const visibleTask = this.filter(this.search(this.state.tasks, this.state.search), this.state.filter)
     return (
       <>
       <div className='App'>
           {console.log('---> render()')}
-          <Header/>
-          <Clock time={this.state.time}/>
+          <Header onChangeFilter={this.onFliterChange}/>
+          {/* <Clock time={this.state.time}/> */}
           <Form
           name='search'
           onHandleSubmit={this.onSearch}
           panelName='Search task'
           submitText='Search'
           iconText='search'
+          validate={false}
           />
           <List
             onChangeAtribute={this.onChangeAtribute}
             onDeleteTask={this.onDeleteTask}
-            taskList={this.state.tasks}
+            taskList={visibleTask}
             onEditTask={this.onEditTask}
           />
           <Form 
@@ -93,6 +129,7 @@ export class App extends Component {
           panelName='Add task'
           submitText='Add task'
           iconText='add'
+          validate={true}
           >
             <SketchPicker inputType='color'/>
           </Form>
--- todolist/src/components/Header/Header.js
+++ todolist/src/components/Header/Header.js
@@ -2,15 +2,35 @@ import React from 'react'
 
 import './Header.css';
 
-export const Header = () => {
+export const Header = ({onChangeFilter}) => {
     return (
     <div className="app__header">
         <p className="app__header__title">Todo</p>
         <div className="app__header__filter-container">
-            <span className="app__header__filter-container__link" >All</span>
-            <span className="app__header__filter-container__link" >Active</span>
-            <span className="app__header__filter-container__link" >Done</span>
-            <span className="app__header__filter-container__link" >Important</span>
+            <span 
+            className="app__header__filter-container__link" 
+            onClick={()=> onChangeFilter('all')}
+            >
+                All
+            </span>
+            <span 
+            className="app__header__filter-container__link" 
+            onClick={()=> onChangeFilter('active')}
+            >
+                Active
+            </span>
+            <span 
+            className="app__header__filter-container__link" 
+            onClick={()=> onChangeFilter('done')}
+            >
+                Done
+            </span>
+            <span 
+            className="app__header__filter-container__link" 
+            onClick={()=> onChangeFilter('important')}
+            >
+                Important
+            </span>
         </div>
     </div>
     )
